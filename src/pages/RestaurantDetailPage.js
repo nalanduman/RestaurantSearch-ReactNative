e98d6b45ce05f4974e579e039cac5c10a43322d6
nalanduman <nalan_dmn@hotmail.com>
2020-08-01 20:25:35 +0300

    Finished

--- src/pages/RestaurantDetailPage.js
+++ src/pages/RestaurantDetailPage.js
@@ -16,27 +16,12 @@ const RestaurantDetailPage = (props) => {
         let {data} = await axios.get("https://opentable.herokuapp.com/api/restaurants/" + props.route.params.restaurantID)
         setList(data)
     }
-
-    // const renderDetail = ({item}) => {
-    //     return(
-    //         <DetailItem 
-    //             detailData={item}   
-    //         />
-    //     )
-    // }
-
+    
     return(
         <SafeAreaView>
             <View>
 
                 <DetailItem detailData={list}/>
-
-                {/* <FlatList 
-                    keyExtractor={(_, index) => index.toString}
-                    data={list}
-                    renderItem={renderDetail}
-                    ItemSeparatorComponent={() => <View style={{marginVertical: 5, borderWidth: 1, borderColor: '#FF5722'}} />}
-                /> */}
 
             </View>
         </SafeAreaView>
